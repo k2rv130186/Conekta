3123bb16636bc850756bad82c5853d4f3cbcec5d
KRISHNA KAREN RAMOS VELAZQUEZ <k2rv130186@hotmail.com>
2020-07-13 11:38:28 -0500

    Comenzar la prueba

---
+++ TechnicalTestConekta/Bussines/Pixel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussines
+{
+    public class Pixel
+    {
+        private int X { get; set;}
+        private int Y { get; set;}
+        private string Color { get; set;}
+        
+
+        public Pixel(int x, int y)
+        {
+            this.X= x;
+            this.Y = y;
+            this.Clear();
+        }
+
+        public void ChangeColor(string color)
+        {
+            this.Color = color;
+        }
+       
+        public void Clear()
+        {
+            this.Color = "O";
+        }
+    }
+}
